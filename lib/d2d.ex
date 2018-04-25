f58aa476f8183390bc307660d2a1911c47a4eb15
Michael Pope <mpope.cr@gmail.com>
2018-04-25 17:22:55 +0200

    Create initial commit

---
+++ lib/d2d.ex
@@ -0,0 +1,18 @@
+defmodule D2D do
+  @moduledoc """
+  Documentation for D2D.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> D2D.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/d2d_test.exs
@@ -0,0 +1,8 @@
+defmodule D2DTest do
+  use ExUnit.Case
+  doctest D2D
+
+  test "greets the world" do
+    assert D2D.hello() == :world
+  end
+end
